4a8816cdf46bb09f5d113a3ffc3c12a3861d36e7
Lucas <lucasgabrielsilvageh@gmail.com>
2025-05-04 01:49:13 -0300

    Alterando fonts

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,15 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 <link rel="stylesheet" href="../../assets/style.css">
+<link rel="stylesheet" href="../../assets/logo-animada.css">
 
 <nav class="navbar navbar-expand-lg navbar-custom">
     <div class="container-fluid d-flex justify-content-between align-items-center">
-        <a class="navbar-brand" href="../view/niveis/formulario-cadastro.php">Dev<span class="highlight">Cadastro</span></a>
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <span class="logo-bar me-2 animate-bar"></span>
+            <i class="bi bi-code-slash me-2 animate-icon"></i>
+            <span class="fw-bold text-white">Dev<span class="highlight">Level</span></span>
+        </a>
         <div class="d-flex">
             <ul class="navbar-nav d-flex flex-row">
                 <li class="nav-item">
